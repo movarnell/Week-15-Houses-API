b63ca73aa2fe497c38a66b83b2c02f62d2e42040
movarnell <movarnell@gmail.com>
2023-03-27 20:49:24 -0500

    added more function conversions

--- src/Components/HousesList.js
+++ src/Components/HousesList.js
@@ -41,3 +41,55 @@ export class HousesList extends React.Component {
                 )
         }
 };
+
+
+// BELOW IS THE FUNCTION FORMAT OF THE ABOVE CODE 
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+// import React, { useState, useEffect } from 'react';
+// import { House } from './House';
+// import { housesApi } from '../rest/HousesApi';
+
+// export const HousesList = () => {
+//   // Initialize component state with an empty array of houses
+//   const [houses, setHouses] = useState([]);
+
+//   // Fetch houses from the API when the component mounts
+//   useEffect(() => {
+//     fetchHouses();
+//   }, []);
+
+//   // Fetch all houses from the API
+//   const fetchHouses = async () => {
+//     try {
+//       const housesData = await housesApi.get();
+//       setHouses(housesData);
+//     } catch (e) {
+//       console.log('Oops, looks like fetchHouses had an issue.', e);
+//     }
+//   }
+
+//   // Update a house in the list of houses and refetch the updated list from the API
+//   const updateHouse = async (updatedHouse) => {
+//     try {
+//       await housesApi.put(updatedHouse);
+//       fetchHouses();
+//     } catch (e) {
+//       console.log('Oops, looks like updating houses had an issue.', e);
+//     }
+//   };
+
+//   return (
+//     <div className='house-list'>
+//       {/* Map over the array of houses in state and render a House component for each */}
+//       {houses.map((house) => (
+//         <House
+//           house={house}
+//           key={house._id}
+//           updateHouse={updateHouse}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+// // 
